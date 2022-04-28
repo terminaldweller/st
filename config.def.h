bfe0d2c75912697977dcd2373974896e7ba093d4
terminaldweller <thabogre@gmail.com>
2022-04-28 11:35:25 +0430

    wide glyph patch

--- config.def.h
+++ config.def.h
@@ -6,10 +6,10 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char font[] =
-    "DejaVuSansMono Nerd Font Mono:pixelsize=14:antialias=true;autohint=true";
+    "DejaVuSansMono Nerd Font Mono:pixelsize=14;antialias=true;autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-    "Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true",
+    "DejaVuSansMono Nerd Font Mono:pixelsize=14;antialias=true;autohint=true",
     "Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true",
 };
 static int borderpx = 2;
