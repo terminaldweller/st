bc8111c77d340c2c0a6fa20eac03f204940da2fa
terminaldweller <thabogre@gmail.com>
2022-04-29 18:36:12 +0430

    increased emoji size

--- config.def.h
+++ config.def.h
@@ -10,7 +10,7 @@ static char font[] =
 /* Spare fonts */
 static char *font2[] = {
     "DejaVuSansMono Nerd Font Mono:pixelsize=14;antialias=true;autohint=true",
-    "Noto Color Emoji:pixelsize=11.3:antialias=true:autohint=true",
+    "Noto Color Emoji:pixelsize=14:antialias=true:autohint=true",
 };
 static int borderpx = 2;
 
